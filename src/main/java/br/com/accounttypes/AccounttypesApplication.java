3552660a0996d8d374918f59ce4e769c1b2f6e21
Rafael Ferreira Caetano <rcaetano0709@gmail.com>
2024-02-12 19:38:51 -0300

    Primeiro commit

---
+++ src/main/java/br/com/accounttypes/AccounttypesApplication.java
@@ -0,0 +1,13 @@
+package br.com.accounttypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccounttypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccounttypesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/accounttypes/AccounttypesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.accounttypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccounttypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
